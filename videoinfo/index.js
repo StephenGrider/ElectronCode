1cef5c217ca59ec9295f1c0805a5bc6121832544
stephengrider <ste.grider@gmail.com>
2017-04-26 11:46:14 -0700

    added ipc sender

--- videoinfo/index.js
+++ videoinfo/index.js
@@ -1,8 +1,12 @@
 const electron = require('electron');
 
-const { app, BrowserWindow } = electron;
+const { app, BrowserWindow, ipcMain } = electron;
 
 app.on('ready', () => {
   const mainWindow = new BrowserWindow({});
   mainWindow.loadURL(`file://${__dirname}/index.html`);
+});
+
+ipcMain.on('video:submit', () => {
+  
 });
