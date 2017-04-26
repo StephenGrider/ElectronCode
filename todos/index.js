8673e31cbdb68fb6b061257858356b5eb4b8262c
stephengrider <ste.grider@gmail.com>
2017-04-26 15:30:20 -0700

    added menu label

--- todos/index.js
+++ todos/index.js
@@ -1,10 +1,22 @@
 const electron = require('electron');
 
-const { app, BrowserWindow } = electron;
+const { app, BrowserWindow, Menu } = electron;
 
 let mainWindow;
 
 app.on('ready', () => {
   mainWindow = new BrowserWindow({});
   mainWindow.loadURL(`file://${__dirname}/main.html`);
+
+  const mainMenu = Menu.buildFromTemplate(menuTemplate);
+  Menu.setApplicationMenu(mainMenu);
 });
+
+const menuTemplate = [
+  {
+    label: 'File',
+    submenu: [
+      { label: 'New Todo' }
+    ]
+  }
+];
