a4cd585043969f3f89241922120b1d9f83842977
stephengrider <ste.grider@gmail.com>
2017-04-26 10:26:47 -0700

    load html doc

--- videoinfo/index.js
+++ videoinfo/index.js
@@ -1,7 +1,8 @@
 const electron = require('electron');
 
-const { app } = electron;
+const { app, BrowserWindow } = electron;
 
 app.on('ready', () => {
-  console.log('App is now ready');
+  const mainWindow = new BrowserWindow({});
+  mainWindow.loadURL(`file://${__dirname}/index.html`);
 });
