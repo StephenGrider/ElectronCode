8380fb15e5348606ceef698d5d54d25f45ba0e6c
stephengrider <ste.grider@gmail.com>
2017-04-26 16:29:55 -0700

    create separate window

--- todos/index.js
+++ todos/index.js
@@ -3,6 +3,7 @@ const electron = require('electron');
 const { app, BrowserWindow, Menu } = electron;
 
 let mainWindow;
+let addWindow;
 
 app.on('ready', () => {
   mainWindow = new BrowserWindow({});
@@ -12,11 +13,22 @@ app.on('ready', () => {
   Menu.setApplicationMenu(mainMenu);
 });
 
+function createAddWindow() {
+  addWindow = new BrowserWindow({
+    width: 300,
+    height: 200,
+    title: 'Add New Todo'
+  });
+}
+
 const menuTemplate = [
   {
     label: 'File',
     submenu: [
-      { label: 'New Todo' },
+      {
+        label: 'New Todo',
+        click() { createAddWindow(); }
+      },
       {
         label: 'Quit',
         accelerator: process.platform === 'darwin' ? 'Command+Q' : 'Ctrl+Q',
