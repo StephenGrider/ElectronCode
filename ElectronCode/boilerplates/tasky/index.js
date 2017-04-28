5b7bb76513a8c986a7fb52a2a225bc766ec2030f
stephengrider <ste.grider@gmail.com>
2017-04-28 13:41:47 -0700

    added tray click handler

--- ElectronCode/boilerplates/tasky/index.js
+++ ElectronCode/boilerplates/tasky/index.js
@@ -4,17 +4,22 @@ const electron = require('electron');
 const { app, BrowserWindow, Tray } = electron;
 
 let mainWindow;
+let tray;
 
 app.on('ready', () => {
   mainWindow = new BrowserWindow({
     height: 500,
     width: 300,
     frame: false,
-    resizable: false
+    resizable: false,
+    show: false
   });
   mainWindow.loadURL(`file://${__dirname}/src/index.html`);
 
   const iconName = process.platform === 'win32' ? 'windows-icon.png' : 'iconTemplate.png';
   const iconPath = path.join(__dirname, `./src/assets/${iconName}`);
-  new Tray(iconPath);
+  tray = new Tray(iconPath);
+  tray.on('click', () => {
+    mainWindow.show();
+  });
 });
