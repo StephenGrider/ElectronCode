96b5cf7319fbd99f81e1cbf2ef6efaba4a04eb0e
stephengrider <ste.grider@gmail.com>
2017-04-28 11:47:49 -0700

    Create tray icon

--- ElectronCode/boilerplates/tasky/index.js
+++ ElectronCode/boilerplates/tasky/index.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const electron = require('electron');
 
-const { app, BrowserWindow } = electron;
+const { app, BrowserWindow, Tray } = electron;
 
 let mainWindow;
 
@@ -13,4 +13,8 @@ app.on('ready', () => {
     resizable: false
   });
   mainWindow.loadURL(`file://${__dirname}/src/index.html`);
+
+  const iconName = process.platform === 'win32' ? 'windows-icon.png' : 'iconTemplate.png';
+  const iconPath = path.join(__dirname, `./src/assets/${iconName}`);
+  new Tray(iconPath);
 });
