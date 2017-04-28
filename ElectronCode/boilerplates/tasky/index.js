df4930625e4f784a28dbd6ce9db0d1fd8caa66a9
stephengrider <ste.grider@gmail.com>
2017-04-28 14:19:33 -0700

    set window position

--- ElectronCode/boilerplates/tasky/index.js
+++ ElectronCode/boilerplates/tasky/index.js
@@ -19,7 +19,24 @@ app.on('ready', () => {
   const iconName = process.platform === 'win32' ? 'windows-icon.png' : 'iconTemplate.png';
   const iconPath = path.join(__dirname, `./src/assets/${iconName}`);
   tray = new Tray(iconPath);
-  tray.on('click', () => {
-    mainWindow.show();
+
+  tray.on('click', (event, bounds) => {
+    // Click event bounds
+    const { x, y } = bounds;
+
+    // Window height and width
+    const { height, width } = mainWindow.getBounds();
+
+    if (mainWindow.isVisible()) {
+      mainWindow.hide();
+    } else {
+      mainWindow.setBounds({
+        x: x - width / 2,
+        y,
+        height,
+        width
+      });
+      mainWindow.show();
+    }
   });
 });
