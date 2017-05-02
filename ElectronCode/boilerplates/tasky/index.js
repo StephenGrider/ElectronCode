b36b6dc85ba573ec62542c0e93b090caa25352ce
stephengrider <ste.grider@gmail.com>
2017-05-02 11:31:43 -0700

    more on gc

--- ElectronCode/boilerplates/tasky/index.js
+++ ElectronCode/boilerplates/tasky/index.js
@@ -5,6 +5,7 @@ const TimerTray = require('./app/timer_tray');
 const { app, BrowserWindow, Tray } = electron;
 
 let mainWindow;
+let tray;
 
 app.on('ready', () => {
   mainWindow = new BrowserWindow({
@@ -18,5 +19,5 @@ app.on('ready', () => {
 
   const iconName = process.platform === 'win32' ? 'windows-icon.png' : 'iconTemplate.png';
   const iconPath = path.join(__dirname, `./src/assets/${iconName}`);
-  new TimerTray(iconPath, mainWindow);
+  tray = new TimerTray(iconPath, mainWindow);
 });
