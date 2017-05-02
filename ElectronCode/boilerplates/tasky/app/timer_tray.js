47f73260db05348ec6fd4419aa1710c86389e5e5
stephengrider <ste.grider@gmail.com>
2017-05-02 11:39:29 -0700

    added context menu

--- ElectronCode/boilerplates/tasky/app/timer_tray.js
+++ ElectronCode/boilerplates/tasky/app/timer_tray.js
@@ -1,5 +1,5 @@
 const electron = require('electron');
-const { Tray } = electron;
+const { Tray, app, Menu } = electron;
 
 class TimerTray extends Tray {
   constructor(iconPath, mainWindow) {
@@ -9,6 +9,7 @@ class TimerTray extends Tray {
 
     this.setToolTip('Timer App');
     this.on('click', this.onClick.bind(this));
+    this.on('right-click', this.onRightClick.bind(this));
   }
 
   onClick(event, bounds) {
@@ -30,6 +31,17 @@ class TimerTray extends Tray {
       });
       this.mainWindow.show();
     }
+  }
+
+  onRightClick() {
+    const menuConfig = Menu.buildFromTemplate([
+      {
+        label: 'Quit',
+        click: () => app.quit()
+      }
+    ]);
+
+    this.popUpContextMenu(menuConfig);
   }
 }
 
