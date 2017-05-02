10fa1dd900f38d0e94a9a612564d3683e36a1c02
stephengrider <ste.grider@gmail.com>
2017-05-02 11:48:27 -0700

    hide on blur

--- ElectronCode/boilerplates/tasky/index.js
+++ ElectronCode/boilerplates/tasky/index.js
@@ -16,6 +16,9 @@ app.on('ready', () => {
     show: false
   });
   mainWindow.loadURL(`file://${__dirname}/src/index.html`);
+  mainWindow.on('blur', () => {
+    mainWindow.hide();
+  });
 
   const iconName = process.platform === 'win32' ? 'windows-icon.png' : 'iconTemplate.png';
   const iconPath = path.join(__dirname, `./src/assets/${iconName}`);
