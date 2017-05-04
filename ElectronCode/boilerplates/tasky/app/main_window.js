f133357ff3b258ccd53c14adf6003539c5b6bc9f
stephengrider <ste.grider@gmail.com>
2017-05-04 10:04:16 -0700

    added mainwindow class

---
+++ ElectronCode/boilerplates/tasky/app/main_window.js
@@ -0,0 +1,23 @@
+const electron = require('electron');
+const { BrowserWindow } = electron;
+
+class MainWindow extends BrowserWindow {
+  constructor(url) {
+    super({
+      height: 500,
+      width: 300,
+      frame: false,
+      resizable: false,
+      show: false
+    });
+
+    this.loadURL(url);
+    this.on('blur', this.onBlur.bind(this));
+  }
+
+  onBlur() {
+    this.hide();
+  }
+}
+
+module.exports = MainWindow;
--- ElectronCode/boilerplates/tasky/index.js
+++ ElectronCode/boilerplates/tasky/index.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const electron = require('electron');
 const TimerTray = require('./app/timer_tray');
+const MainWindow = require('./app/main_window');
 
 const { app, BrowserWindow, Tray } = electron;
 
@@ -9,17 +10,7 @@ let tray;
 
 app.on('ready', () => {
   app.dock.hide();
-  mainWindow = new BrowserWindow({
-    height: 500,
-    width: 300,
-    frame: false,
-    resizable: false,
-    show: false
-  });
-  mainWindow.loadURL(`file://${__dirname}/src/index.html`);
-  mainWindow.on('blur', () => {
-    mainWindow.hide();
-  });
+  mainWindow = new MainWindow(`file://${__dirname}/src/index.html`);
 
   const iconName = process.platform === 'win32' ? 'windows-icon.png' : 'iconTemplate.png';
   const iconPath = path.join(__dirname, `./src/assets/${iconName}`);
