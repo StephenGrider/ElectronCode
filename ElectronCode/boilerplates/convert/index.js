d003b5c1a121ec092da3405146667e2ea4534669
stephengrider <ste.grider@gmail.com>
2017-05-09 10:35:46 -0700

    start up webpack

--- ElectronCode/boilerplates/convert/index.js
+++ ElectronCode/boilerplates/convert/index.js
@@ -2,10 +2,13 @@ const electron = require('electron');
 
 const { app, BrowserWindow } = electron;
 
+let mainWindow;
+
 app.on('ready', () => {
-  new BrowserWindow({
+  mainWindow = new BrowserWindow({
     height: 600,
     width: 800,
     webPreferences: { backgroundThrottling: false }
   });
+  mainWindow.loadURL(`file://${__dirname}/src/index.html`);
 });
