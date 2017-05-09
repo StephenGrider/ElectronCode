faa9336d018c48475a94eb92f4e729a39cb7dcb2
stephengrider <ste.grider@gmail.com>
2017-05-09 12:58:40 -0700

    add support for multiple videos

--- ElectronCode/boilerplates/convert/index.js
+++ ElectronCode/boilerplates/convert/index.js
@@ -1,5 +1,6 @@
 const electron = require('electron');
 const ffmpeg = require('fluent-ffmpeg');
+const _ = require('lodash');
 
 const { app, BrowserWindow, ipcMain } = electron;
 
@@ -15,11 +16,14 @@ app.on('ready', () => {
 });
 
 ipcMain.on('videos:added', (event, videos) => {
-  const promise = new Promise((resolve, reject) => {
-    ffmpeg.ffprobe(videos[0].path, (err, metadata) => {
-      resolve(metadata);
+  const promises = _.map(videos, video => {
+    return new Promise((resolve, reject) => {
+      ffmpeg.ffprobe(video.path, (err, metadata) => {
+        resolve(metadata);
+      });
     });
   });
 
-  promise.then((metadata) => { console.log(metadata); })
+  Promise.all(promises)
+    .then((results) => console.log(results));
 });
